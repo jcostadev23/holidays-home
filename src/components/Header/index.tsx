1eef5414eb4c1243ba405c26455df5d89e1159f0
jcostadev23 <carlos_jesus_costa@hotmail.com>
2025-06-21 08:49:35 +0100

    First commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <Image
+      aria-hidden
+      src="/madeira-home-logo.png"
+      alt="Madeira Home logo"
+      width={360}
+      height={360}
+    />
+  );
+};
+
+export default Header;
